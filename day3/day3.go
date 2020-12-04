8739254a39c41d3a0b93909e24d087587187bd31
Pat Dale <patdale216@gmail.com>
2020-12-03 17:28:24 -0800

    Day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -4,17 +4,30 @@ import (
     "fmt"
 )
 
-func Run(entries []string) {
+func treesHit(topography []string, rightBy int, downBy int) int {
     treesHit := 0
     xCoordinate := 0
-    for _, topographyLine := range entries {
-        if (xCoordinate >= len(topographyLine)) {
-            xCoordinate = xCoordinate - len(topographyLine)
+    yCoordinate := 0
+    for yCoordinate < len(topography) {
+        if (xCoordinate >= len(topography[yCoordinate])) {
+            xCoordinate = xCoordinate - len(topography[yCoordinate])
         }
-        if (topographyLine[xCoordinate] == '#') {
+        if (topography[yCoordinate][xCoordinate] == '#') {
             treesHit ++
         }
-        xCoordinate += 3
+        xCoordinate += rightBy
+        yCoordinate += downBy
     }
-    fmt.Println(treesHit)
+    return treesHit
+}
+
+func Run(topography []string) {
+    slopes := [][]int{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
+
+    total := 1
+    for _, slope := range slopes {
+        treesHit := treesHit(topography, slope[0], slope[1])
+        total *= treesHit
+    }
+    fmt.Println(total)
 }
